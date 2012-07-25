4391d138158bbcf5df0274285d785b7d26590075
Adam Nowak <adam.nowak@netguru.pl>
2012-07-25 16:04:12 +0200

    stories stored in object, so it doesn't fetch pivotal every time

--- lib/pivo_flow/pivotal.rb
+++ lib/pivo_flow/pivotal.rb
@@ -9,38 +9,47 @@ module PivoFlow
     end
 
     def user_stories
-      fetch_stories(5, user_name)
+      project_stories.select{ |story| story.owned_by == user_name }
+    end
+
+    def project_stories
+      @options[:stories] ||= fetch_stories
     end
 
     def unasigned_stories
-      fetch_stories(5)
+      project_stories.select{ |story| story.owned_by == nil }
     end
 
     def current_story force = false
       if (@options[:current_story] && !force)
         @options[:current_story]
       else
-        stories = fetch_stories(1, user_name, "started")
-        @options[:current_story] = stories.count.zero? ? user_stories.first : stories.first
+        @options[:current_story] = user_stories.count.zero? ? nil : user_stories.first
       end
     end
 
     def list_stories_to_output stories
       HighLine.new.choose do |menu|
-        menu.prompt = "Which one would you like to start?   "
-        stories.each { |story| menu.choice("[##{story.id}] #{story.name}\n\t#{story.description}") { |answer| puts "thanks for picking ##{answer.match(/\[#(?<id>\d+)\]/)[:id]}"} }
+        menu.header = "--- STORIES FROM PIVOTAL TRACKER ---\nWhich one would you like to start?   "
+        menu.prompt = "story no.? "
+        menu.select_by = :index
+        stories.each { |story| menu.choice("[##{story.id}] (requested by: #{story.requested_by}) #{story.name}\n\t#{story.description}") { |answer| puts "thanks for picking ##{answer.match(/\[#(?<id>\d+)\]/)[:id]}"} }
       end
 
     end
 
     def show_stories
-      stories = user_stories.count.zero? ? unasigned_stories : user_stories
-      list_stories_to_output stories
+      stories = user_stories
+      if stories.count.zero?
+        puts "hmm... there is no story assigned to you! I'll better check for unasigned stories!"
+        stories = unasigned_stories
+      end
+      list_stories_to_output stories.last(5)
     end
 
-    def fetch_stories(count = 1, owned_by = nil, state = "unstarted")
-      conditions = { current_state: state, limit: count, owned_by: owned_by }
-      @options[:project].stories.all(conditions)
+    def fetch_stories(count = 100, state = "unstarted")
+      conditions = { current_state: state, limit: count }
+      @options[:stories] = @options[:project].stories.all(conditions)
     end
 
   end

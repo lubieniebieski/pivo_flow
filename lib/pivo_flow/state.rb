a6162227413437196ad1c3c7653fc36966b228d8
David Balatero <dbalatero@gmail.com>
2018-12-26 12:39:55 -0800

    Fix branches that have slashes in them

--- lib/pivo_flow/state.rb
+++ lib/pivo_flow/state.rb
@@ -1,6 +1,6 @@
 module PivoFlow
   module State
-    extend self
+    module_function
 
     def current_branch_name
       Grit::Repo.new(Dir.pwd).head.name
@@ -11,7 +11,9 @@ module PivoFlow
     end
 
     def current_story_id_file_path
-      File.join(story_id_tmp_path, current_branch_name)
+      flat_branch_name = current_branch_name.gsub('/', '--')
+
+      File.join(story_id_tmp_path, flat_branch_name)
     end
 
     def current_story_id
